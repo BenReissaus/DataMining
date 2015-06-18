8a002023742015b6189fea16d1f0bb3505f77c8c
Benjamin Reissaus <benjamin.reissaus@student.hpi.uni-potsdam.de>
2015-06-18 21:47:44 +0200

    add plotting of histogramm for Ex.5 4f

--- Exercise5/Neubert_Reissaus_Week5.R
+++ Exercise5/Neubert_Reissaus_Week5.R
@@ -4,27 +4,26 @@ setwd("/Users/Benji/Documents/Uni/Master/3.Semester/Data_Mining_Probabilistic_Re
 # 4 f
 
 bet <- function(){
-  
-  # create probability distribution
-  
+    
   # set money to be 1€
   betMoney = 1
   wonMoney = 0
-  throws = 0
+  totalBets = 0
   highestBet = 0
+  biggestLoss = 0
   
   winningBetSizes = c()
   while(wonMoney != 3000){
     
     # put money in the middle
     wonMoney = wonMoney - betMoney
+    if(wonMoney < biggestLoss){ biggestLoss = wonMoney}
     
     # throw ball 
     fieldColor = sample(c("red","black","green"), 1, prob=c(18/37,18/37, 1/37), replace = T)
-    throws = throws + 1
+    totalBets = totalBets + 1
     
     if(fieldColor == "red"){
-      print(paste(c("fieldColor: ", fieldColor), collapse = " "))
       
       winningBetSizes = c(winningBetSizes, betMoney)
       wonMoney = wonMoney + (betMoney * 2)
@@ -35,11 +34,14 @@ bet <- function(){
     else {
       betMoney = betMoney * 2
     }
-    print(paste(c("won money: ", wonMoney), collapse = " "))
   }
-  print("winning bet sizes")
-  print(winningBetSizes)
-  print(paste(c("throws: ", throws, "highestBet: ", highestBet), collapse = " "))
+  
+#   print(paste(c("total bets: ", totalBets, "highest bet: ", highestBet, " highest loss: ", biggestLoss), collapse = " "))
+#   hist(winningBetSizes, xlim=c(0,3000), xlab="occurrences", ylab="Winning Bet Size", main="Winning Bet Sizes")
+  
+  library(ggplot2)
+  dfr <- data.frame(x = winningBetSizes)
+  ggplot(dfr, aes(x)) + geom_histogram() + scale_x_continuous(trans="log2") + xlab("Winning Bet Size") + ylab("Count") + ggtitle("Winning Bet Sizes Histogram")
 }
 
 bet()
